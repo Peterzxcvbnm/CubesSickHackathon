1af23b21fcd30bfec323c6a45891b552d5205f4f
Peterzxcvbnm <peter.braendgaard@gmail.com>
2022-10-05 10:38:46 +0200

    JsonContent

---
+++ RobotController/RobotController/RobotInterface/InstantAction.cs
@@ -0,0 +1,9 @@
+﻿namespace SafelogSimulator;
+
+public class InstantAction
+{
+    public string ActionName { get; set; }
+    public int ActionId { get; set; }
+    public string BlockingType { get; set; }
+    public List<ActionParameter> ActionParameters { get; set; }
+}
--- RobotController/RobotController/RobotInterface/RobotController.cs
+++ RobotController/RobotController/RobotInterface/RobotController.cs
@@ -1,12 +1,40 @@
-﻿namespace RobotController.RobotInterface;
+﻿using System.Net.Http.Json;
+using SafelogSimulator;
+
+namespace RobotController.RobotInterface;
 
 public class RobotController : IRobot
 {
+    private HttpClient _client = new HttpClient();
+    private string _robotUrl = Environment.GetEnvironmentVariable("ROBOT_URL") ?? "10.10.10.20";
     private int currentPos = 0;
     
-    public Task Goto(int index)
+    public async Task Goto(int index)
     {
+        if (currentPos == index) return;
         currentPos = index;
-        return Task.CompletedTask;
+        var content = JsonContent.Create(new RootDto()
+        {
+            InstantActions = new List<InstantAction>
+            {
+                new()
+                {
+                    ActionName = "goto",
+                    ActionId = 0,
+                    BlockingType = "NONE",
+                    ActionParameters = new List<ActionParameter>()
+                    {
+                        new()
+                        {
+                            Key = "end",
+                            Value = index.ToString()
+                        }
+                    }
+                }
+            }
+        });
+        var response = await _client.PostAsync(_robotUrl + "api/instantActions", content);
+
+        response.EnsureSuccessStatusCode();
     }
 }
